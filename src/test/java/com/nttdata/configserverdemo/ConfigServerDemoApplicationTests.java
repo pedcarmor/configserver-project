d9098265b58f2d151fab4d53c9be64099e5a42b4
pedcarmor <pcarvajm@emeal.nttdata.com>
2022-02-07 13:20:13 +0100

    Server config project

---
+++ src/test/java/com/nttdata/configserverdemo/ConfigServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.configserverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
